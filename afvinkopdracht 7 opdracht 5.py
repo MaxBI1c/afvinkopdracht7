340f402c8eecb1f36109d60c475a26de0b1f07a6
MaxBI1c <90347510+MaxBI1c@users.noreply.github.com>
2021-10-16 15:51:54 +0200

    Add files via upload

---
+++ afvinkopdracht 7 opdracht 5.py
@@ -0,0 +1,21 @@
+
+def open():
+    '''open het bestand zodat je het kan lezen'''
+bestandsnaam = input('wat is de naam van het bestand?')
+open_bestand = open(bestandsnaam,'r')
+
+return open_bestand
+
+def list():
+
+'''leest het bestand  en split de regels wanneer je eeen komma tegenkomt'''
+komma = []
+for line in bestand:
+komma.append()
+line = (line.split(','))
+print(line.split(','))
+return
+def gemiddelde():
+
+def main():
+main()
